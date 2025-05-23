8ca15d44a620a6248d93b04cc0ad01573013f7e8
Wedsels <fear857@gmail.com>
2025-05-23 15:57:35 -0600

    MCM integration, modularity, stat values

--- Mods/Scaling Difficulty/ScriptExtender/Lua/BootstrapServer.lua
+++ Mods/Scaling Difficulty/ScriptExtender/Lua/BootstrapServer.lua
@@ -1,7 +1,74 @@
 local Key = "Scaling Difficulty"
+local Started = false
+local Downscaling = false
 local PartyLevel = 0
 
-local function CheckMaxLevel()
+local function DCCPY( org )
+    local copy = {}
+    for k,v in pairs( org ) do
+        if type( v ) == "table" then
+            copy[ k ] = DCCPY( v )
+        else
+            copy[ k ] = v
+        end
+    end
+    return copy
+end
+
+local function Whole( n )
+    if n < 0.0 then
+        return math.floor( n )
+    end
+    return math.ceil( n )
+end
+
+--- @class Boost
+--- @field Proficiency number
+--- @field HP number
+--- @field Initiative number
+--- @field Physical number
+--- @field Casting number
+--- @field Strength number
+--- @field Dexterity number
+--- @field Constitution number
+--- @field Intelligence number
+--- @field Wisdom number
+--- @field Charisma number
+
+--- @class Entity
+--- @field Level number
+--- @field Constitution string
+--- @field Physical string
+--- @field Casting string
+--- @field Boost Boost
+
+--- @type table< string, Entity >
+local Scalers = {}
+
+local Scaling = {
+    HP = 8.0,
+    Initiative = 1.0,
+    Physical = 1.5,
+    Casting = 1.5,
+    Strength = 0,
+    Dexterity = 0,
+    Constitution = 0,
+    Intelligence = 0,
+    Wisdom = 0,
+    Charisma = 0
+}
+
+local function DefaultBoost()
+    local ret = { Proficiency = 0.0 }
+    for k,v in pairs( Scaling ) do
+        ret[ k ] = 0.0
+    end
+    return ret
+end
+
+local function CheckMaxLevel( force )
+    if not Started then return end
+
     local high = 0
     for _,p in pairs( Osi.DB_Players:Get( nil ) ) do
         local level = Osi.GetLevel( p[ 1 ] )
@@ -10,7 +77,7 @@ local function CheckMaxLevel()
         end
     end
 
-    if high > PartyLevel then
+    if force or high > PartyLevel then
         PartyLevel = high
 
         for _,ent in pairs( Ext.Entity.GetAllEntities() ) do
@@ -19,31 +86,85 @@ local function CheckMaxLevel()
             if not eoc or not id then goto continue end
 
             local sch = ent:GetComponent( "ServerCharacter" )
-            local player = sch and sch.PlayerData
+            if sch and sch.PlayerData then goto continue end
 
             local uuid = id.EntityUuid
-            if not player and eoc.Level < PartyLevel then
+
+            if not Scalers[ uuid ] then
                 local stats = ent:GetComponent( "Stats" )
                 if not stats then goto continue end
 
-                local change = PartyLevel - eoc.Level
+                Scalers[ uuid ] = {
+                    Level = eoc.Level,
+                    Boost = DefaultBoost(),
+                    Constitution = stats.AbilityModifiers[ 4 ],
+                    Physical = stats.Abilities[ 2 ] <= stats.Abilities[ 3 ] and "Dexterity" or "Strength",
+                    Casting = tostring( stats.SpellCastingAbility )
+                }
+            end
 
-                local phys = "Strength"
-                if stats.Abilities[ 2 ] <= stats.Abilities[ 3 ] then
-                    phys = "Dexterity"
+            local scale = Scalers[ uuid ]
+
+            local undown = not Downscaling and Scalers[ uuid ].Boost.HP ~= 0 and scale.Level > PartyLevel
+
+            if scale.Level < PartyLevel or Downscaling and scale.Level > PartyLevel or undown then
+                local change = PartyLevel - scale.Level
+
+                local prev = DCCPY( scale.Boost )
+                if undown then
+                    scale.Boost = DefaultBoost()
+                else
+                    for k,v in pairs( Scaling ) do
+                        if k == "HP" then
+                            scale.Boost[ k ] = Whole( ( v + scale.Constitution ) * change )
+                        else
+                            scale.Boost[ k ] = Whole( v * change )
+                        end
+                    end
+                    scale.Boost.Proficiency = Whole( PartyLevel / 4.0 + 1.0 )
                 end
 
-                local boost =
-                    "ProficiencyBonusOverride( " .. math.ceil( eoc.Level / 4 ) + 1 .. " );" ..
-                    "IncreaseMaxHP( " .. ( 8 + stats.AbilityModifiers[ 4 ] ) * change .. " );" ..
-                    "Initiative( " .. change .. " );" ..
-                    "Ability( " .. phys .. ", " .. change * 2 .. " );" ..
-                    "Ability( " .. tostring( stats.SpellCastingAbility ) .. ", " .. change * 2 .. " )"
+                local once = false
 
-                Osi.AddBoosts( uuid, boost, Key, "" )
+                local remove = ""
+                local add = ""
+                for k,v in pairs( scale.Boost ) do
+                    if ( v ~= 0.0 or prev[ k ] ~= 0.0 ) and v ~= prev[ k ] then
+                        local fun = ""
+                        if k == "Proficiency" then
+                            fun = "ProficiencyBonusOverride( "
+                        elseif k == "HP" then
+                            fun = "IncreaseMaxHP( "
+                        elseif k == "Initiative" then
+                            fun = "Initiative( "
+                        elseif k == "Physical" then
+                            fun = "Ability( " .. scale.Physical .. ", "
+                        elseif k == "Casting" then
+                            fun = "Ability( " .. scale.Casting .. ", "
+                        else
+                            fun = "Ability( " .. k .. ", "
+                        end
 
-                eoc.Level = PartyLevel
-                ent:Replicate( "EocLevel" )
+                        if prev[ k ] then
+                            remove = remove .. fun .. prev[ k ] .. " );"
+                        end
+                        add = add .. fun .. v .. " );"
+
+                        once = true
+                    end
+                end
+
+                if once then
+                    Osi.RemoveBoosts( uuid, remove, 0, Key, "" )
+                    Osi.AddBoosts( uuid, add, Key, "" )
+
+                    if undown then
+                        eoc.Level = scale.Level
+                    else
+                        eoc.Level = PartyLevel
+                    end
+                    ent:Replicate( "EocLevel" )
+                end
             end
 
             :: continue ::
@@ -51,5 +172,36 @@ local function CheckMaxLevel()
     end
 end
 
-Ext.Osiris.RegisterListener( "LevelGameplayStarted", 2, "after", CheckMaxLevel )
-Ext.Osiris.RegisterListener( "LeveledUp", 1, "after", CheckMaxLevel )
+if MCM then
+    local function SetValues()
+        for k,v in pairs( Scaling ) do
+            Scaling[ k ] = MCM.Get( k )
+        end
+
+        Downscaling = MCM.Get( "Downscaling" )
+    end
+
+    SetValues()
+
+    Ext.ModEvents.BG3MCM[ "MCM_Setting_Saved" ]:Subscribe(
+        function( payload )
+            if not payload or payload.modUUID ~= ModuleUUID or not payload.settingId then
+                return
+            end
+
+            SetValues()
+            CheckMaxLevel( true )
+        end
+    )
+end
+
+Ext.Osiris.RegisterListener(
+    "LevelGameplayReady",
+    2,
+    "after",
+    function ( ... )
+        Ext.Osiris.RegisterListener( "LeveledUp", 1, "after", CheckMaxLevel )
+        Started = true
+        CheckMaxLevel()
+    end
+)
--- README.md
+++ README.md
@@ -5,13 +5,19 @@ Each level an enemy gets scaled, they will gain:
 - The proper Proficiency Bonus for their new level
 - An HP increase of 8 + Consitution Modifier
 - 1 additional Initiative
-- 2 bonus to their highest physical stat ( Strength or Dexterity )
-- 2 bonus to their Spellcasting Ability ( Intelligence, Wisdom, or Charisma )
+- 1.5 bonus to their highest physical stat ( Strength or Dexterity )
+- 1.5 bonus to their Spellcasting Ability ( Intelligence, Wisdom, or Charisma )
 - No change in rewarded experience
 
+Optional, but recommended compatability with [ MCM ]( https://www.nexusmods.com/baldursgate3/mods/9162 )
+With MCM:
+- Change the scaling values for any of the above mentioned
+- Allow enemies to be additionally downscaled to the party level
+- Tweak each individual ability score bonus per level
+
 Works alongside any mod, and takes effect on any mod-added enemies.
+Does scale any NPC ally or enemy
 Does not scale any players or recruitable characters.
-Does scale any NPC ally or enemy.
 
 Requires Script Extender.
 Load order does not matter, place anywhere.
